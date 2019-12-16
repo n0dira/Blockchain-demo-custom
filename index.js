24b6d4f27dd9448cbdf55dbbe2db8d6cdcb62820
Azmir Shah <52946862+n0dira@users.noreply.github.com>
2019-12-16 03:04:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var $ = require('jquery');
 var jsdom = require('jsdom');
 var CryptoJS = require('crypto-js')
-var pattern = '0000';
+var pattern = '0000000000';
 var nonce = 0;
 var maximumNonce = 1099511627776;
-var difficulty = 4;
+var difficulty = 10;
 var hash = (CryptoJS.SHA256(1 + '' + nonce + data));
 var hashhex = (hash.toString(CryptoJS.enc.Hex));
 var data = "Kosar A Bokhari0000000000000000000000000000000000000000000000000000000000000000";
 
 function mine() {
-  for (var x = 0; x <= maximumNonce; x++) {
+  for (var x = 68719476736; x <= maximumNonce; x++) {
     var nonce = x;
     var hash = (CryptoJS.SHA256(1 + '' + nonce + data));
     var hashhex = (hash.toString(CryptoJS.enc.Hex));
@@ -21,22 +21,3 @@ function mine() {
    console.log(nonce);
   }
 }
-
-
-
-
-
-
-
-
-//function mine() {
-  //for (var x = 0; x <= maximumNonce; x++) {
-    //hash = (CryptoJS.HmacSHA256(data, nonce));
-    //nonce = (x);
-    //console.log(nonce);
-   // if (alert(hash.toString(CryptoJS.enc.Hex).substring(0, difficulty) === pattern)) {
-     // console.log("success!:"(nonce))
-     // break;
-  //  }
- // }
-//}
